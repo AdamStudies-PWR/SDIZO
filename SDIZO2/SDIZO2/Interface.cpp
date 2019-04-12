532177d473402d7d6b49d204f770cc72e9bf1e9e
Adam Krizar <apocalypsearisen44@gmail.com>
2019-04-12 15:18:27 +0200

    Initial commit

---
+++ SDIZO2/SDIZO2/Interface.cpp
@@ -0,0 +1,26 @@
+﻿//Implementacja metod klasy Interface
+#include "pch.h"
+#include "Interface.h"
+
+using namespace std;
+
+//Główne menu programu
+void Interface::main_menu()
+{
+	char choice;
+	do
+	{
+		system("cls");
+		cout << "\t---Główne Menu---" << endl;
+		cout << " [1] Graf" << endl;
+		cout << " [0] Zakończ program" << endl;
+		cout << " Wybór: ";
+		choice = _getche();
+		switch (choice)
+		{
+		case '0': break;
+		case '1': cout << "\nTBA" << endl, _getch(); break;
+		default: cout << "Błąd wprowadzenia, spróbuj ponowne" << endl; _getch();
+		}
+	} while (choice != '0');
+}
---
+++ SDIZO2/SDIZO2/Interface.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef Interface_hpp
+#define Interface_hpp
+// Kasa obs³uguj¹ca wszsytkie interakcje u¿ytkownika z aplikacj¹œ
+
+using namespace std;
+
+class Interface
+{
+public:
+	//metody
+	//G³ówne menu propgramu
+	void main_menu();
+};
+
+#endif
